2246b5f33b3d0736804bef88b6bb8441d14a365f
Ademir Júnior <adejr0220@gmail.com>
2024-01-25 10:39:52 -0300

    Projeto Job Fider: configuração do banco de dados e rotas, edição do arquivo Notas.txt

---
+++ Job Finder/app.js
@@ -0,0 +1,34 @@
+const express       = require('express');
+const app           = express();
+const db            = require('./db/connection');
+const bodyParser    = require('body-parser');
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor conectado na porta ${PORT}`);
+});
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+// DB Connection
+
+db.authenticate()
+  .then(() => {
+    console.log('Conexão com Banco de Dados estabelecida!');
+  })
+  .catch(err => {
+    console.log(`[db.authenticate() ERROR]: ${err}`);
+  });
+
+
+// Routes
+
+app.get('/', (req,res) => {
+    res.send('Projeto funcionando')    
+});
+
+// Job routes
+app.use('/jobs', require('./routes/jobs'))
---
+++ Job Finder/routes/jobs.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+/* // testing
+router.get('/test', (req,res) => {
+    res.send('Jobs Routes working!');
+}) */
+
+// Add Job via POST
+router.post('/add', (req,res) => {
+
+    /* Através do body-parser, fazemos o mapeamento dos atributos obtidos 
+    pelo corpo da requisição */
+    let { title, description, salary, company, email, is_new } = req.body;
+
+    Job.create({ 
+        title, 
+        description, 
+        salary, 
+        company, 
+        email, 
+        is_new 
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log('[Job.create() ERROR]:\n' + err));
+});
+
+module.exports = router;
